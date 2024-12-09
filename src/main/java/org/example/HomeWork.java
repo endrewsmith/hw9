c5edbf97f1958f91cc858f8762c5a0005310efc9
Andrew Kuznetsov <56806222+endrewsmith@users.noreply.github.com>
2024-12-09 19:13:36 +0300

    Update HomeWork.java

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -1,10 +1,8 @@
 package org.example;
 
-
 import lombok.SneakyThrows;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class HomeWork {
 
@@ -15,8 +13,69 @@ public class HomeWork {
     @SneakyThrows
     public void upit(InputStream in, OutputStream out) {
 
+        if (in == null) {
+            throw new IllegalArgumentException("Входящий поток не может быть null");
+        }
+        if (out == null) {
+            throw new IllegalArgumentException("Выходящий поток не может быть null");
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        PrintWriter pw = new PrintWriter(out);
+
+        String[] first = br.readLine().split(" ");
+
+        int n = Integer.parseInt(first[0]);
+        int q = Integer.parseInt(first[1]);
+        long[] a = new long[200001];
+
+
+        String[] strings = br.readLine().split(" ");
+        for (int i = 1; i <= n; i++) {
+            a[i] = Long.parseLong(strings[i - 1]);
+        }
+
+        while (q > 0) {
+            q--;
+            String[] command = br.readLine().split(" ");
+            int operation = Integer.parseInt(command[0]);
+            int x = Integer.parseInt(command[1]);
+            int y = Integer.parseInt(command[2]);
+            long z = Long.parseLong(command[3]);
+            switch (operation) {
+                // update
+                case 1:
+                    for (int i = x; i <= y; i++) {
+                        a[i] = z;
+                    }
+                    break;
+                // add
+                case 2:
+                    long sum_2 = 0;
+                    for (int i = x; i <= y; i++) {
+                        sum_2 += z;
+                        a[i] += sum_2;
+                    }
+                    break;
+                // insert
+                case 3:
+                    int u = Integer.parseInt(command[1]);
+                    long l = Long.parseLong(command[2]);
+                    n++;
+                    for (int i = n; i > u; i--) {
+                        a[i] = a[i - 1];
+                    }
+                    a[u] = l;
+                    break;
+                // sum
+                case 4:
+                    long sum_4 = 0;
+                    for (int i = x; i <= y; i++) {
+                        sum_4 += a[i];
+                    }
+                    pw.println(sum_4);
+                    break;
+            }
+        }
     }
-
-
-
 }
